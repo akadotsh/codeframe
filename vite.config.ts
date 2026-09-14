774742d5f54f21b9f2fe64f4be5f65b575ae17b8
akadotsh <devakash256@gmail.com>
2026-09-14 15:17:58 +0530

    build: configure StyleX compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
+import stylex from "@stylexjs/unplugin";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [stylex.vite(), react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./", import.meta.url)),
